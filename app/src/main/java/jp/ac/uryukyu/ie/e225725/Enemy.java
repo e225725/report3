04623230841d026e1e99143519350d5c7281f5b6
applerm <e225725@ie.u-ryukyu.ac.jp>
2023-02-24 05:24:56 +0900

    report4 step1

--- app/src/main/java/jp/ac/uryukyu/ie/e225725/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e225725/Enemy.java
@@ -1,70 +1,11 @@
 package jp.ac.uryukyu.ie.e225725;
 
-//import org.checkerframework.checker.units.qual.A;
-
-/**
- * 敵クラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
- */
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
-
-    /**
-     * コンストラクタ。名前、最大HP、攻撃力を指定する。
-     * @param name モンスター名
-     * @param maximumHP モンスターのHP
-     * @param attack モンスターの攻撃力
-     */
-    public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+public class Enemy extends LivingThing{
+    public Enemy(String name, int hitPoint, int attack){
+        super(name, hitPoint, attack);
     }
 
-    public String getName(){
-        return name;
-    }
-
-    public boolean getDead(){
-        return dead;
-    }
-
-    public int getHitPoint(){
-        return this.hitPoint;
-    }
-
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if (hitPoint<=0){
-            dead=true;
-        }
-        if (this.dead == false){
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
-    }
-
-    /**
-     * 自身へ攻撃されたときのダメージ処理をするメソッド。
-     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-     * @param damage 受けたダメージ
-     */
-    public void wounded(int damage){
+    @Override public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
             dead = true;
